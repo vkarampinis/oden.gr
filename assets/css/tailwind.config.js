dcb62df8e2ef17a4d71e81c9c872ff0827ed79fc
Vaios Karampinis <oden@oden.gr>
2020-04-25 21:43:03 +0300

    modify tailwind config

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   theme: {
-    extend: {}
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        my: {
+          c1: "#FCFBF8",
+          c2: "#F8B595",
+          c3: "#F67280",
+          c4: "#C06C84",
+          c5: "#6C5B7C",
+          b1: "#333333",
+        },
+      },
+    },
   },
   variants: {},
-  plugins: []
-}
+  plugins: [],
+};
